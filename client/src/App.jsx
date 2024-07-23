c6e8e43c8a7b61355b71aa098c40512edbcaa7b5
Dan Sexton <danielsexton1999@gmail.com>
2024-07-22 20:46:17 -0400

    working auth component & server bug fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import Auth from "./components/Auth/Auth";
+import { Route, Routes, useLocation } from "react-router-dom";
+import MainIndex from "./components/Main/MainIndex";
+import "./App.css";
+import LoginPrompt from "./components/Auth/LoginPrompt";
 
 function App() {
- 
+  //token functionality put into App for ease of use for other members of the team
+  const [token, setToken] = useState(localStorage.getItem("token"));
+  const location = useLocation(); // grabbing location
+
+  const handleTokenUpdate = (newToken) => {
+    setToken(newToken);
+    localStorage.setItem("token", newToken);
+  };
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      setToken(token);
+    }
+  }, []);
+
   return (
     <>
-     
+      <Routes>
+        <Route
+          path="/auth"
+          element={<Auth onTokenUpdate={handleTokenUpdate} />}
+        />
+      </Routes>
+      {token ? (
+        <MainIndex token={token} />
+      ) : (
+        location.pathname !== "/auth" && <LoginPrompt /> // preventing the LoginPrompt from showing on /auth
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/Main/MainIndex.jsx
@@ -0,0 +1,8 @@
+export default function MainIndex({ token }){
+     return (
+    <div>
+      <h1>Welcome to the Main Page</h1>
+      <p>Your token: {token}</p>
+    </div>
+  );
+};
