fd6ced206ea3c1c1628f1a81ce29ca773a28cd74
Dan Sexton <danielsexton1999@gmail.com>
2024-07-24 19:29:13 -0400

    Update MainIndex.jsx

--- client/src/components/Main/MainIndex.jsx
+++ client/src/components/Main/MainIndex.jsx
@@ -3,6 +3,7 @@ export default function MainIndex({ token }){
     <div>
       <h1>Welcome to the Main Page</h1>
       <p>Your token: {token}</p>
+      {/* replace this with the actual main page code or delete and update the call in App.jsx to display the correct page */}
     </div>
   );
 };
